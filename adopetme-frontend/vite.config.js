d670e69ebb6352b433cdbf6c191e2b2a23f7fae2
mathlori <math.louren@gmail.com>
2025-10-20 22:52:57 -0300

    a

--- adopetme-frontend/vite.config.js
+++ adopetme-frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prototipo_adopetme/',
+  base: '/Prototipo_Adopetme/',
   plugins: [
     tailwindcss(),
     react()
